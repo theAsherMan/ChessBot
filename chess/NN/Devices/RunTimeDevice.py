efa5a7e12d1230495af70c7e927e4a9ad253ca14
theAsherMan <162021136+theAsherMan@users.noreply.github.com>
2025-12-18 10:07:56 +0800

    fixed devices again

--- chess/NN/Devices/RunTimeDevice.py
+++ chess/NN/Devices/RunTimeDevice.py
@@ -4,9 +4,9 @@ import torch
 available_devices = []
 
 if os.environ.get("COLAB_TPU_ADDR"):
-    available_devices.append(torch.device('tpu'))
+    available_devices.append(torch.device('xla'))
 if torch.cuda.is_available():
-    available_devices.append(torch.device('gpu'))
+    available_devices.append(torch.device('cuda'))
 available_devices.append(torch.device('cpu'))
 
 best_device = available_devices[0]
