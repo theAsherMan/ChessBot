103cb0e5aa1bb47e9d9d05032eb04aac663891f9
theAsherMan <162021136+theAsherMan@users.noreply.github.com>
2025-12-18 13:03:10 +0800

    more debugging

--- chess/ChessBots/Models/SimpleUNet.py
+++ chess/ChessBots/Models/SimpleUNet.py
@@ -97,7 +97,10 @@ class SimpleUNet(Module):
     def backward(self, boards:list[Board], move_eval:list[np.ndarray]):
         self.train()
         move_preds = self.forward(boards)
-        move_eval = torch.tensor(np.array(move_eval)).to(self.device).argmax(dim=1)
+        move_preds = move_preds.flatten(start_dim=1)
+        move_eval = torch.tensor(np.array(move_eval)).to(self.device)
+        move_eval = move_eval.flatten(start_dim=1)
+        move_eval = move_eval.argmax(dim=1)
         print(f'move predictiona:{move_preds}')
         print(f'move lable:{move_eval}')
         move_loss = self.loss_fn(move_preds, move_eval.argmax(dim=1))
